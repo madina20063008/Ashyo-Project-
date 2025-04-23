fd04a500e8c3c3096270b83767f9ee28572dc1e0
madina20063008 <madina.batosheva@gmail.com>
2025-04-23 04:16:35 -0700

    ashyo

--- components/Button.tsx
+++ components/Button.tsx
@@ -3,10 +3,12 @@ import React, { FC } from 'react';
 
 const Button: FC<ButtonType> = ({ icon, iconPosition, title, extraStyle }) => {
   return (
-    <button className={ 'bg-[#134E9B] text-white ${extrastyle} font-medium text-[16px] py-[15px] px-[25px] flex items-center justify-center gap-[20px] cursor-pointer hover:opacity-80 duration-300 rounded-[6px]' }>
-      {icon && iconPosition === "left" && icon}
+    <button
+      className={`bg-[#134E9B] text-white ${extraStyle} font-medium text-[16px] py-[15px] px-[25px] flex items-center justify-center gap-[20px] cursor-pointer hover:opacity-80 duration-300 rounded-[6px]`}
+    >
+      {icon && iconPosition === 'left' && icon}
       {title}
-      {icon && iconPosition === "right" && icon}
+      {icon && iconPosition === 'right' && icon}
     </button>
   );
 };
